88a729e7780b772a525c787af9d56c3a4066ad4b
Kamran <muhammadkamran5862@gmail.com>
2022-12-20 20:20:16 +0500

    message

---
+++ PD 2 tasks/t3.txt
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"A"<<endl;
+cout<<"X"<<endl;
+cout<<"Z"<<endl;
+cout<<"K"<<endl;
+cout<<"S"<<endl;
+cout<<"U"<<endl;
+return 0;
+}
---
+++ PD 2 tasks/t4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"  PPP PP        AA            CC CC      MM        MM           AA          NNN        NN       "<<endl;
+cout<<"   PP  PP     AA AA         CC      CC      MM  MM            AA  AA         NN N      NN       "<<endl;
+cout<<"   PP  PP    AA   AA        CC           MM   MM   MM        AA    AA        NN  N     NN       "<<endl;
+cout<<"   PP  PP    AA AA AA       CC           MM    M   MM       AA  AA  AA       NN   N    NN       "<<endl;
+cout<<"   PP PP    AA      AA      CC           MM        MM       AA      AA       NN    N   NN       "<<endl;
+cout<<"   PP      AA        AA     CC           MM        MM      AA        AA      NN     N  NN       "<<endl;
+cout<<"   PP      AA         AA    CC      CC   MM        MM     AA          AA     NN      N NN       "<<endl; 
+cout<<"   PP     AA          AA      CC CC      MM        MM    AA            AA    NN        NN       "<<endl;
+return 0;
+}
